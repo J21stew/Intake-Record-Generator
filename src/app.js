0a72c11af049dc32c0f07e32bf1e5dbfab721718
J21stew <j.lebron.stewart@gmail.com>
2026-05-21 11:09:37 -0400

    Message generator

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
+const messageInput = document.getElementById("message-input");
+const generateButton = document.getElementById("generate-button");
+const recordOutput = document.getElementById("record-output");
 
+generateButton.addEventListener("click", function() {
+    const message = messageInput.value;
+    recordOutput.innerHTML = `
+    <h2>Message Received:</h2>
+    <p>${message}</p>
+    `;
+});
